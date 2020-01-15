d270cf62448e1090232cedb3868f0dcff8c11bab
rphanley <rphanley@rocketmail.com>
2020-01-15 12:57:16 +0000

    Up to cart.html

--- products/models.py
+++ products/models.py
@@ -6,3 +6,6 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to= 'images')
+
+    def __str__(self):
+        return self.name
